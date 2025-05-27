458466478737820a3ab4e9a5416f3f3dcc5fa7d2
alvesgabrieel <gabriel_alvesqm@hotmail.com>
2025-05-27 17:05:10 -0300

    fix: update white theme as default

--- src/components/mode-toogle.tsx
+++ src/components/mode-toogle.tsx
@@ -40,9 +40,6 @@ export function ModeToggle() {
         <DropdownMenuItem onClick={() => setTheme("dark")}>
           Dark
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
