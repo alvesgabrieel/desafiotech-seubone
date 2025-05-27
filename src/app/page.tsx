e84b5f7f54a483967d7b14a4971e89cb5434125a
alvesgabrieel <gabriel_alvesqm@hotmail.com>
2025-05-27 20:11:57 -0300

    feat: enforce auth redirects on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
+import { Loader } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+import { useAuthStore } from "@/store/authStore";
 
 export default function Home() {
+  const router = useRouter();
+  const { isAuthenticated } = useAuthStore();
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      router.push("/login");
+    } else {
+      router.push("/dashboard");
+    }
+  }, [isAuthenticated, router]);
+
   return (
-    <div>
-      <Button variant="destructive">Teste</Button>
+    <div className="flex h-screen w-screen items-center justify-center">
+      <Loader className="h-10 w-10 animate-spin text-gray-500" />
     </div>
   );
 }
